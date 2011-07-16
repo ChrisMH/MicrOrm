419207291274eceb578a5727b1e5a5903f78bf37
Chris Hogan <hogancm@gmail.com>
2011-07-16 09:33:05 -0400

    Added ConnectionProvider

---
+++ MicrORM/ConnectionProvider.cs
@@ -0,0 +1,27 @@
+﻿namespace MicrORM
+{
+  public class ConnectionProvider : IConnectionProvider
+  {
+    /// <summary>
+    /// Create using a named connection string setting from the application configuration file
+    /// </summary>
+    /// <param name="connectionName">The name of the connection in the application configuration file</param>
+    public ConnectionProvider(string connectionName)
+    {
+
+    }
+
+    /// <summary>
+    /// Create using a connection string and provider name
+    /// </summary>
+    /// <param name="connectionString">The connection string</param>
+    /// <param name="providerName">The database provider name</param>
+    public ConnectionProvider( string connectionString, string providerName )
+    {
+      
+    }
+
+    protected string ConnectionString { get; private set; }
+    protected DbProviderFacory ProviderFactory { get; private set; }
+  }
+}
---
+++ MicrORM/IConnectionProvider.cs
@@ -0,0 +1,9 @@
+﻿using System.Data.Common;
+
+namespace MicrORM
+{
+  public interface IConnectionProvider
+  {
+    DbConnection CreateConnection();
+  }
+}
