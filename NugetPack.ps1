3b17ba3bc898cb01f274dfbe2aed76c5db063ce7
Chris Hogan <chogan@logikos.com>
2012-03-07 13:50:46 -0500

    Updated NUnit, moved nuspecs, updated build script

--- NugetPack.ps1
+++ NugetPack.ps1
@@ -1,12 +1,28 @@
-$srcRoot = '.\src'                     # relative to script directory
-$versionFile = 'SharedAssemblyInfo.cs' # relative to $srcRoot
+[string[]] $buildFiles = 
+  '.\src\MicrOrm\MicrOrm.csproj'
+[string[]] $nuspecFiles = 
+  '.\nuspec\MicrOrm.nuspec'
+  
+$versionFile = '.\src\SharedAssemblyInfo.cs'
+
+$buildConfiguration = 'Release'
 $outputPath = "$home\Dropbox\Packages"
 
-Import-Module NugetUtilities
+Import-Module BuildUtilities
 
+$version = Get-Version (Resolve-Path $versionFile)
+  
 New-Path $outputPath
 
-$version = Get-Version (Join-Path $srcRoot $versionFile)
+#foreach($buildFile in $buildFiles)
+#{
+#  Invoke-Build (Resolve-Path $buildFile) $buildConfiguration
+#}
 
-Pack-Project MicrOrm $srcRoot $version $outputPath
+foreach($nuspecFile in $nuspecFiles)
+{
+  New-Package (Resolve-Path $nuspecFile) $version $outputPath
+}
+
+Remove-Module BuildUtilities
 
