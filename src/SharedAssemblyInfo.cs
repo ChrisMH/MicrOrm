adc90808fe1c8157f051d90c88cb32dfb8c3489d
Chris Hogan <hogancm@gmail.com>
2016-02-04 19:40:25 -0500

    Added .pdb files to release builds

--- src/SharedAssemblyInfo.cs
+++ src/SharedAssemblyInfo.cs
@@ -9,6 +9,6 @@ using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("1.0.2.2")]
-[assembly: AssemblyFileVersion("1.0.2.2")]
-[assembly: AssemblyInformationalVersion("1.0.2.2")]
+[assembly: AssemblyVersion("1.0.2.3")]
+[assembly: AssemblyFileVersion("1.0.2.3")]
+[assembly: AssemblyInformationalVersion("1.0.2.3")]
