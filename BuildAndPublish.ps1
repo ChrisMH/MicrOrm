2bd25c4eefe6d5ed2a7bfa18c483d6a03a4a68b4
Chris Hogan <hogancm@gmail.com>
2016-09-16 11:37:44 -0400

    Rearranged source, updated packages

--- BuildAndPublish.ps1
+++ BuildAndPublish.ps1
@@ -4,6 +4,9 @@
 # -Build 
 #   Build the projects
 #
+# -Clean 
+#   Cleans output files
+#
 # -Pack 
 #   Package the projects 
 #
@@ -29,25 +32,24 @@ $solution = "MicrOrm.sln"         #relative to $srcRoot
   "MicrOrm452\MicrOrm452.csproj",
   "MicrOrm46\MicrOrm46.csproj",
   "MicrOrm461\MicrOrm461.csproj"
+  
+[string[]] $testProjects =          #relative to $srcRoot
+  "MicrOrm45.Test\MicrOrm45.Test.csproj"
 
 [string[]] $packages = 
   "MicrOrm"
 
-$srcRoot = ".\src"                      # relative to script directory
+$srcRoot = "."                          # relative to script directory
 $outputPath = ".\out"                   # relative to script directory
+$packagesPath = ".\packages"            # relative to script directory
 $nuspecPath = ".\nuspec"                # relative to script directory
 $versionFile = 'SharedAssemblyInfo.cs'  # relative to $srcRoot
 
 $repository = "http://nuget.hogancode.com/Hogan/nuget"
 $apiKey = "Chris051010"
 
-
 $buildCmd = "$env:windir\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe"
 $nugetCmd = "${Env:ProgramFiles(x86)}\nuget\nuget.exe"
-
-
-# Create the output path if it does not exist
-if((Test-Path $outputPath) -eq $False) { New-Item -Path $outputPath -ItemType directory | Out-Null }
 
 # Get version information from AssemblyInformationalVersion key in the assembly info file
 $matchPattern = "^\[\s*assembly:\s*AssemblyInformationalVersion\s*\(\s*\""(?<version>.*)\""\s*\)\s*\]"
@@ -74,16 +76,42 @@ if($args -contains "-Build")
         Write-Host "`nBuilding $project from $projectFile`n" -ForegroundColor Green
 
         &$buildCmd $projectFile /p:Configuration=Release /t:clean 
-        if($LastExitCode) {throw "Build Failed"}
+        if($LastExitCode) {throw "Clean Failed"}
         &$buildCmd $projectFile /p:Configuration=Release /t:rebuild 
         if($LastExitCode) {throw "Build Failed"}
     }
 }
 
+# Clean all output files
+if($args -contains "-Clean")
+{    
+    Write-Host "`Removing output directories...`n" -ForegroundColor Green
+
+    foreach($project in $projects + $testProjects)
+    {
+        $projectFile = Join-Path $srcRoot $project
+        if((Test-Path $projectFile) -eq $False) { throw "Could not find project file for '$project'" }
+
+		$projectDir = Split-Path -Path $projectFile -Parent
+
+		$binDir = Join-Path $projectDir "bin"
+		if((Test-Path $binDir) -eq $True) { Remove-Item -Recurse -Force $binDir }
+		
+		$objDir = Join-Path $projectDir "obj"
+		if((Test-Path $objDir) -eq $True) { Remove-Item -Recurse -Force $objDir }
+    }
+	
+
+    if((Test-Path $outputPath) -eq $True) { Remove-Item -Recurse -Force $outputPath }
+    if((Test-Path $packagesPath) -eq $True) { Remove-Item -Recurse -Force $packagesPath }
+}
 
 # Package all projects
 if($args -contains "-Pack")
 {   
+	# Create the output path if it does not exist
+	if((Test-Path $outputPath) -eq $False) { New-Item -Path $outputPath -ItemType directory | Out-Null }
+
     Write-Host "`nPackaging to $outputPath...`n" -ForegroundColor Green
 
     foreach($package in $packages)
---
+++ SharedAssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyCompany("Chris Hogan")]
+[assembly: AssemblyProduct("MicrOrm")]
+[assembly: AssemblyCopyright("Copyright © Chris Hogan 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion("1.0.5.0")]
+[assembly: AssemblyFileVersion("1.0.5.0")]
+[assembly: AssemblyInformationalVersion("1.0.5.0")]
