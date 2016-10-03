a7bbdb4a4a01a4aef1cdfca320682af463b8f9eb
Chris Hogan <hogancm@gmail.com>
2016-10-03 08:34:29 -0400

    Updated dependencies, added packages

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -9,6 +9,6 @@ using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("1.1.1")]
-[assembly: AssemblyFileVersion("1.1.1")]
-[assembly: AssemblyInformationalVersion("1.1.1")]
+[assembly: AssemblyVersion("1.1.2")]
+[assembly: AssemblyFileVersion("1.1.2")]
+[assembly: AssemblyInformationalVersion("1.1.2")]
